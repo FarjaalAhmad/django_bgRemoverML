9fcf2e415ae9b36e1aca10a29b1033a905027904
Farjaal Ahmad <farjaalahmad@gmail.com>
2020-10-17 18:38:35 +0500

    Added Setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 wget http://download.tensorflow.org/models/deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz
 wget http://download.tensorflow.org/models/deeplabv3_pascal_train_aug_2018_01_04.tar.gz
 
