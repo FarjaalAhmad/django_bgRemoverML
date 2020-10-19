1f45de6320cba6da8222163e25141b0331496456
Farjaal Ahmad <farjaalahmad@gmail.com>
2020-10-19 13:19:14 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,6 +21,6 @@ mv u2net.pth ../models/u2net/
 mv u2netp.pth ../models/u2netp/
 
 cd ../
-rm -rf setup*
 rm deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz
 rm deeplabv3_pascal_train_aug_2018_01_04.tar.gz
+rm -rf setup*
